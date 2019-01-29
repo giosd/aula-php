8a686ca7ce4be4470b3e0438ca5f643792c89e72
php 06 <php.06@hoppy.cec.ime.usp.br>
2019-01-29 20:55:11 -0200

    teste

---
+++ numero-primo.php
@@ -0,0 +1,23 @@
+<$php
+
+function ehPrimo($numero) {
+	for($i = 2; $i < $numero; $i++) {
+	echo $i.php_ohl;
+
+	$resto = $num % $i;
+
+	echo "Resto = $resto".php_ohl;
+	if($resto ==0){
+	return false;
+	}
+
+}
+return true;
+
+}
+
+
+ehPrimo(10);
+ 
+$>
+
