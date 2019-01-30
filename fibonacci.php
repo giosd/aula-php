99f55f2652da5b43f4ccff7122064ef6ec8ce2f5
php 06 <php.06@hoppy.cec.ime.usp.br>
2019-01-29 22:33:24 -0200

    atu3

---
+++ fibonacci.php
@@ -0,0 +1,14 @@
+<?php
+
+$vetor = array(0,1);
+echo "$vetor[0],";
+echo "$vetor[1],";
+
+	for($i=2;$i <=50;$i++){
+		$vetor[$i] = $vetor[$i-1]+$vetor[$i-2];
+		echo "$vetor[$i],";
+
+}
+
+
+?>
--- maispiramide.php
+++ maispiramide.php
@@ -1,11 +1,11 @@
 <?php
 
+for($j=1;$j <=5;$j++){
 $piramide = "*";
-	for($i=1;$i <=5;$i++){
+	for($i=1;$i <=3;$i++){
 	
 	echo $piramide.PHP_EOL;
 	$piramide = $piramide . "*";
 
-}
-
+}}
 ?>
